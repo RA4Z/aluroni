7315c482f664dac956f668868e2ca2409dfaa337
RA4Z <rraz639@gmail.com>
2023-05-09 13:14:07 -0300

    Limitando o código de Opções para evitar erros

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -4,9 +4,10 @@ import opcoes from './opcoes.json';
 import classNames from 'classnames';
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md'
 
+export type OpcoesOrdenador = '' | 'porcao' | 'qtd_pessoas' | 'preco';
 interface Props {
-    ordenador: string;
-    setOrdenador: React.Dispatch<React.SetStateAction<string>>
+    ordenador: OpcoesOrdenador;
+    setOrdenador: React.Dispatch<React.SetStateAction<OpcoesOrdenador>>
 }
 
 export default function Ordenador({ ordenador, setOrdenador}: Props) {
@@ -31,7 +32,7 @@ export default function Ordenador({ ordenador, setOrdenador}: Props) {
                     <div 
                         className={styles.ordenador__option} 
                         key={opcao.value} 
-                        onClick={() => setOrdenador(opcao.value)}>
+                        onClick={() => setOrdenador(opcao.value as OpcoesOrdenador)}>
                         {opcao.nome}
                     </div>
                 ))}
