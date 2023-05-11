275d92263ec2aaa418d1c3742a94ab085b2e6573
RA4Z <rraz639@gmail.com>
2023-05-11 12:21:50 -0300

    Reaproveitando o Header

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,24 @@
 import Menu from 'components/Menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import styles from './Routes.module.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
     return (
-        <Router>
-            <Menu />
-            <Routes>
-                <Route path='/' element={<Inicio />} />
-                <Route path='/cardapio' element={<Cardapio />} />
-            </Routes>
-        </Router>
+        <main>
+            <Router>
+                <Menu />
+                <header className={styles.header}>
+                    <div className={styles.header__text}>
+                        A casa do código e da massa
+                    </div>
+                </header>
+                <Routes>
+                    <Route path='/' element={<Inicio />} />
+                    <Route path='/cardapio' element={<Cardapio />} />
+                </Routes>
+            </Router>
+        </main>
     );
 }
