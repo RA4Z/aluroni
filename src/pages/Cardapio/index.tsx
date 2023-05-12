e6ecebc2bc27b7f015311044c5262ebe8f919bef
RA4Z <rraz639@gmail.com>
2023-05-12 12:07:32 -0300

    Adicionando o estilo tema para refatorar o index da página padrão

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,6 @@
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
 import { useState } from 'react';
 import Filtros from './Filtros';
 import Ordenador, { OpcoesOrdenador } from './Ordenador';
@@ -11,7 +12,7 @@ export default function Cardapio() {
     const [ordenador, setOrdenador] = useState<OpcoesOrdenador>('');
     return(
         <section className={styles.cardapio}>
-            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+            <h3 className={stylesTema.titulo}>Cardápio</h3>
             <Buscador 
                 busca={busca} 
                 setBusca={setBusca} 
