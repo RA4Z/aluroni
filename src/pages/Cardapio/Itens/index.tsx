aa0e2e92b5ce35f390fe2d53c457fc0e2736a6fd
RA4Z <rraz639@gmail.com>
2023-05-17 12:23:49 -0300

    Alterando os tipos do projeto do cardápio

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -2,6 +2,7 @@ import cardapio from 'data/cardapio.json';
 import Item from './Item';
 import styles from './Itens.module.scss';
 import { useEffect, useState } from 'react';
+import { Cardapio } from 'types/Prato';
 
 interface Props {
     busca: string;
@@ -23,11 +24,11 @@ export default function Itens(props: Props) {
         return true;
     }
 
-    function ordenarPropriedadeCrescente(lista: typeof cardapio, propriedade: 'size' | 'serving' | 'price' ) {
+    function ordenarPropriedadeCrescente(lista: Cardapio, propriedade: 'size' | 'serving' | 'price' ) {
         return lista.sort((a,b) => a[propriedade] > b[propriedade] ? 1 : -1);
     }
 
-    function ordenar(novaLista: typeof cardapio) {
+    function ordenar(novaLista: Cardapio) {
         switch(ordenador) {
         case 'porcao':
             return ordenarPropriedadeCrescente(novaLista,'size');
